6d57ab36159c6bee278477245cff034318f269f0
edouardLaffont <edouard.laffont@live.com>
2022-11-24 10:30:59 +0100

    entity initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PictureModule } from './picture/picture.module';
+import { ProfilModule } from './profil/profil.module';
+import { SearchModule } from './search/search.module';
+import { LikeModule } from './like/like.module';
+import { ReportModule } from './report/report.module';
+import { ConversationModule } from './conversation/conversation.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PictureModule, ProfilModule, SearchModule, LikeModule, ReportModule, ConversationModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
